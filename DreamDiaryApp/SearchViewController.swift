933bdd0f8641cef660d1351bba9206645e5aa68a
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-09-30 18:02:55 +0900

    修正

---
+++ DreamDiaryApp/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  DreamDiaryApp
+//
+//  Created by Ao Nagasawa on 2020/09/30.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
