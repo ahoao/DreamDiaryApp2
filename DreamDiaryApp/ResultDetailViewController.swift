80b0643935f97dd19424af5a1585ee8bed5b55f9
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-10-28 16:22:30 +0900

    検索結果詳細製作中（詳細が出ない）

--- DreamDiaryApp/ResultDetailViewController.swift
+++ DreamDiaryApp/ResultDetailViewController.swift
@@ -12,44 +12,40 @@ class ResultDetailViewController: UIViewController {
     @IBOutlet weak var dreamFeelingTagImage: UIImageView!
     @IBOutlet weak var favoriteDreamImage: UIImageView!
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewDidLoad()
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewDidLoad()}
+    
+    var displayImageNo = 0
+    var imageNameArray = ["feeling1after","feeling2after","feeling3after","feeling4after","feeling5after","feeling6after",]
+    
+    let realm = try! Realm()
+           var diary: Diary!
+    
+    func displayImage(displayImageNo: Int) {
         
-        var displayImageNo = 0
-        var imageNameArray = ["feeling1after","feeling2after","feeling3after","feeling4after","feeling5after","feeling6after",]
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNo]
         
-        func displayImage(displayImageNo: Int) {
-            
-            // 表示している画像の番号から名前を取り出し
-            let name = imageNameArray[displayImageNo]
-            
-            // 画像を読み込み
-            let selectedImage = UIImage(named: name)
-            
-            // Image Viewに読み込んだ画像をセット
-            dreamFeelingTagImage.image = selectedImage
-        }
+        // 画像を読み込み
+        let selectedImage = UIImage(named: name)
         
-        //     fav表示
-        func favImage(favImage: Bool){
-            if !favImage{
-                favoriteDreamImage.image = nil
-                return
-            }
-            let selectedImage = UIImage(named: "favAfter")
-            
-            // Image Viewに読み込んだ画像をセット
-            favoriteDreamImage.image = selectedImage
-        }
+        // Image Viewに読み込んだ画像をセット
+        dreamFeelingTagImage.image = selectedImage
     }
     
-    
-    
+    //     fav表示
+    func favImage(favImage: Bool){
+        if !favImage{
+            favoriteDreamImage.image = nil
+            return
+        }
+        let selectedImage = UIImage(named: "favAfter")
+        
+        // Image Viewに読み込んだ画像をセット
+        favoriteDreamImage.image = selectedImage
+    }
     
     func detail(){
-        let realm = try! Realm()
-        var diary: Diary!
-        
         var result = realm.objects(Diary.self)
         for resultData in result {
             dreamDateLabel.text = diary.date
@@ -58,12 +54,5 @@ class ResultDetailViewController: UIViewController {
             displayImage(displayImageNo: resultData.feelingTag)
             favImage(favImage: resultData.favoriteDream)
         }
-        
-        
     }
 }
-
-
-
-
-
